73c7522311e0ce8af6ea4e55a50348d090e8311e
Trevor Huffman <trevorhuffman09@gmail.com>
2022-07-08 13:37:48 -0500

    add navigation with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+//routes tell the router which view to display when a user
+//clicks a link or pastes a URL into the browser address bar.
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
+];
 
+//initializes the router and starts it listening for browser location changes.
 @NgModule({
+  //following line adds the RouterModule to the AppRoutingModule imports array and 
+  //configures it with the routes in one step by calling RouterModule.forRoot()
   imports: [RouterModule.forRoot(routes)],
+  //exporting this makes it available throughout the app
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
